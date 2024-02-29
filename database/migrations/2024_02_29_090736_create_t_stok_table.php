51563421432a1e18cd0193aadace386706f0f754
Echa Aprilya <echaaprilya044@gmail.com>
2024-02-29 16:38:12 +0700

    t_stok

--- database/migrations/2024_02_29_090736_create_t_stok_table.php
+++ database/migrations/2024_02_29_090736_create_t_stok_table.php
@@ -15,13 +15,12 @@ return new class extends Migration
             $table->id('stok_id');
             $table->unsignedBigInteger('barang_id')->index();
             $table->unsignedBigInteger('user_id')->index();
-            $table->datetime('stok_tanggal')->unique();
-            $table->int('stok_jumlah');
+            $table->datetime('stok_tanggal');
+            $table->integer('stok_jumlah');
             $table->timestamps();
 
-            $table->foreign('user
-            
-            _id')->references('user_id')->on('m_user');
+            $table->foreign('barang_id')->references('barang_id')->on('m_barang');
+            $table->foreign('user_id')->references('user_id')->on('m_user');
         });
     }
 
