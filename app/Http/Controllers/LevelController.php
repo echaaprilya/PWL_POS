84c06941147c4cb680768f297b87c028940febd3
Echa Aprilya <echaaprilya044@gmail.com>
2024-03-02 22:32:52 +0700

    Praktikum4

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,9 +9,17 @@ class LevelController extends Controller
 {
     public function index()
     {
-        DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUST', 'Pelanggan', now()]);
+        //DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        //return 'Insert data baru berhasil';
 
-     return 'Insert data baru berhasil';
+        //$row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['customer', 'CUS']);
+        //return 'update data berhasil. jumlah data yang diupdate:' .$row. 'baris';
+
+        //$row = DB::delete('delete from m_level where level_kode = ?', ['CUST']);
+        //return 'Delete data berhasil. Jumlah data yang dihapus: ' .$row. ' baris';
+
+        $data = DB::select('select * from m_level');
+        return view('level', ['data' => $data]);
     }
     
 }
