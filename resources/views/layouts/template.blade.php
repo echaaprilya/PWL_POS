fd123a587e9bd2e0648d5d1552cd71796d628be7
Echa Aprilya <echaaprilya044@gmail.com>
2024-04-04 19:04:45 +0700

    Pertemuan7_Praktikum3

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -5,31 +5,39 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'PWL Laravel Starter Code') }}</title>
 
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.css') }}">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('template/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('template/plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('template/plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('template/dist/css/adminlte.min.css') }}">
+
+  @stack('css') <!-- Digunakan untuk memanggil custom css dari perintah push('css') pada masing-masing view -->
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
-  <!--Naavbar-->
-  @include('layouts.header')
-  <!-- /.navbar -->
+    <!-- Navbar -->
+    @include('layouts.header')
+    <!-- /.navbar -->
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ url('/')}}" class="brand-link">
-      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">PWL - Starter code</span>
+    <a href="{{ url('/') }}" class="brand-link">
+      <img src="{{ asset('template/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
-    <!--sidebaar-->
+    <!-- Sidebar -->
     @include('layouts.sidebar')
-    <!-- /.sidebar-->
+    <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
@@ -39,23 +47,39 @@
 
     <!-- Main content -->
     <section class="content">
-      @yield('content')
+        @yield('content')
     </section>
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
 
-  @include('layouts.footer')
+ @include('layouts.footer')
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('template/plugins/jquery/jquery.min.js') }}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('template/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<!-- DataTables & Plugin -->
+<script src="{{asset('template/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{asset('template/plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{asset('template/plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{asset('template/plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{asset('template/plugins/datatables-buttons/js/buttons.colvis.min.js') }}"></script>
 <!-- AdminLTE App -->
-<script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="{{ asset('adminlte/dist/js/demo.js') }}"></script>
+<script src="{{ asset('template/dist/js/adminlte.min.js') }}"></script>
+<script>
+  // Untuk mengirimkan token Laravel CSRF
+  $.ajaxSetup({headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}});
+  </script>
+  @stack('js') <!--Digunakan untuk memanggil custom js dari perintah push('js')-->
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,14 @@ Route::delete('/kategori/{id}/delete', [KategoriController::class, 'delete']);
 
 //per7
 Route::get('/', [WelcomeController::class,'index']);
+
+Route::group(['prefix' => 'user'], function(){
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/list', [UserController::class, 'list']);
+    Route::get('/create', [UserController::class, 'create']);
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{id}', [UserController::class, 'show']);
+    Route::get('/{id}/edit', [UserController::class, 'edit']);
+    Route::put('/{id}', [UserController::class, 'update']);
+    Route::delete('/{id}', [UserController::class, 'destroy']);
+});
