d663c1ac124b09d53ef8a4952ddb84eb56e8051e
Echa Aprilya <echaaprilya044@gmail.com>
2024-04-05 10:02:14 +0700

    Pertemuan7_Tugas(Level)

--- app/Models/LevelModel.php
+++ app/Models/LevelModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LevelModel extends Model
 {
@@ -11,4 +12,14 @@ class LevelModel extends Model
 
     protected $table = 'm_level';
     protected $primaryKey = 'level_id';
+
+    protected $fillable = [
+        'level_kode',
+        'level_nama',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(UserModel::class);
+    }
 }
